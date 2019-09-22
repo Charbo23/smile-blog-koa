26ee283a31e897e743a73b3b2a925a5319d0eed7
charbo23 <charbo233@qq.com>
2019-09-22 15:19:23 +0800

    加入leanengine依赖

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const InitManager = require('./core/init')
 const catchError = require('./middleware/exception')
 const cors = require('koa2-cors');
 const multipart = require('./core/multipart')
+const AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID,
+  appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+});
+
+// 如果不希望使用 masterKey 权限，可以将下面一行删除
+AV.Cloud.useMasterKey();
 
 const app = new Koa()
 
